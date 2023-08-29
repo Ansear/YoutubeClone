54881eed89428b35d437125d013fbc999fd45d16
sebastianarciniegas2003 <sebastianarciniegas2003@gmail.com>
2023-08-29 09:38:41 -0500

    feat:🚀: Responsive de la vista Principal youtube

--- src/script.js
+++ src/script.js
@@ -6,7 +6,7 @@ menu.addEventListener("click",()=>{
     side.classList.toggle("w-[5%]");
     side.classList.toggle("mb-6");
     side.classList.toggle("af");
-    hr.classList.toggle("w-2/6")
-    hr.classList.toggle("mb-6")
-    cont.classList.toggle("p-[20px_2%_20px_7%]")
+    hr.classList.toggle("w-2/6");
+    hr.classList.toggle("mb-6");
+    cont.classList.toggle("p-[20px_2%_20px_7%]");
 })
