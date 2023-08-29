bd38993b14638ff68e5088a98b82aa356bbacd5a
sebastianarciniegas2003 <sebastianarciniegas2003@gmail.com>
2023-08-29 17:51:19 -0500

    feat:✨: consumir el json de detalles del video
    Se modificaron los archivos playVideo.html y play.js para mostar la informacion del video

--- src/play.js
+++ src/play.js
@@ -53,5 +53,36 @@ fra.insertAdjacentHTML("afterbegin",/*html*/`
 (async()=>{
     let peticion = await fetch("../data/dataVideoDetail.json")
     let res = await peticion.json()
+    let deta = document.querySelector("#details")
+    let chanel = document.querySelector("#channel")
+    deta.insertAdjacentHTML("beforeend",
+        /*html */`
+        <h3 class="text-xl font-semibold">${res.title}</h3>
+        <div class="flex items-center justify-between flex-wrap mt-2.5 text-sm text-[#5a5a5a]">
+            <p>${res.stats.views}views &bull; ${res.publishedDate}</p>
+            <div>
+                <a class="inline-flex items-center ml-4" href=""><img class="w-5 mr-2" src="../images/like.png" alt="">${res.stats.likes}</a>
+                <a class="inline-flex items-center ml-4" href=""><img class="w-5 mr-2" src="../images/dislike.png" alt=""></a>
+                <a class="inline-flex items-center ml-4" href=""><img class="w-5 mr-2" src="../images/share.png" alt="">Share</a>
+                <a class="inline-flex items-center ml-4" href=""><img class="w-5 mr-2" src="../images/save.png" alt="">Save</a>
+            </div>
+        </div>`
+    )
+    chanel.insertAdjacentHTML("beforeend",/*html*/`
+    <div class="flex w-full">
+        <img class=" rounded-3xl mr-4" src=${dataChannel.avatar[0].url} >
+        <div class="flex-[1]">
+            <p class="text-black font-semibold text-lg">${dataChannel.title}</p>
+            <span class="text-xs text-[#5a5a5a]">${dataChannel.stats.subscribers} Subscribers</span>
+        </div>
+        <button class="bg-red-600 text-white py-2 px-8 border-none outline-[0] rounded-3xl cursor-pointer " type="submit">Subscribe</button>
+    </div>
+    `)
     
 })();
+console.log(dataChannel)
+
+/*html*/`
+<p class="text-sm mb-1 text-[#5a5a5a]">Channel that makes learning easy</p>
+<p class="text-sm mb-1 text-[#5a5a5a]">Subscribe Easy Tutorials to watch more Tutorials on web Development</p>
+`
