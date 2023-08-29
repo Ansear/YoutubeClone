d6c23f2fac6d36abd6ec33cddbdbe71bff25c180
sebastianarciniegas2003 <sebastianarciniegas2003@gmail.com>
2023-08-29 16:30:10 -0500

    feat:🚧: Implementando funcionalidad del buscador

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
 let menu = document.querySelector("#MenuIcon")
-let result = {};
+let dataChannel = {};
 //funcionalidad del sidebar menu
 let side = document.querySelector("#sidebar")
 let hr = document.querySelector("#hrI")
@@ -16,7 +16,7 @@ menu.addEventListener("click",()=>{
 //Funcionalidad para traerme la info del canal
 (async()=>{
     let peti = await fetch("../data/dataChannel.json");
-    result = await peti.json();
+    dataChannel = await peti.json();
 })();
 
 //Funcionalidad para cargar videos en la vista home
@@ -30,10 +30,10 @@ menu.addEventListener("click",()=>{
         <div id="cardV" idV="${value.video.videoId}">
             <a href="./playVideo.html"><img class="w-full rounded-xl" src=${value.video.thumbnails[2].url}></a>
             <div class="flex items-start mt-2">
-                <img class="w-9 mr-3 rounded-3xl" src="${result.avatar[0].url}">
+                <img class="w-9 mr-3 rounded-3xl" src="${dataChannel.avatar[0].url}">
                 <div class="text-[#5a5a5a] text-xs block mb-1">
                     <a class="text-black" href="">${value.video.title}</a>
-                    <p>${result.title}</p>
+                    <p>${dataChannel.title}</p>
                     <p>${value.video.stats.views} Views &bull; ${value.video.publishedTimeText}</p>
                 </div>
 
@@ -41,5 +41,53 @@ menu.addEventListener("click",()=>{
         </div>
     `).join(" ")
     }`)
-    
+    let cards = document.querySelectorAll("#cardV");
+    cards.forEach(e=>{
+        e.addEventListener("click",()=>{
+            idV = e.getAttribute("idV");
+            localStorage.setItem("ID VIDEO", idV);
+        })
+    })
+})();
+
+//Funcionalidades para las sugerencias del buscador
+(async()=>{
+    let peticion = await fetch("../data/dataVideos.json");
+    let res = await peticion.json();
+    document.querySelector("#chatSearch").addEventListener("change", (e)=>{
+        if(e.target.value){
+            document.querySelector("#boxSearch").style.borderRadius = "15px";
+            document.querySelector("#resultOut").style.display = "none";
+        }else{
+            document.querySelector("#boxSearch").style.borderRadius = "15px 15px 0px 0px";
+            (async()=>{
+                let peti = await fetch("../data/dataVideos.json");
+                let res = await peti.json();
+                let h=0,cont =0;
+                let array = res.contents.map((val,id)=>{
+                    if(val.playlist) return undefined;
+                    else{
+                        cont++
+                    }
+                    if(cont<=10) h = 30 * cont;
+
+                    return /*html*/`<a href="./playVideo.html" id="element" idV="${val.video.videoId}"><li>${val.video.title}</li></a>
+            ${console.log(val.video.title)};`
+
+                })
+                document.querySelector("#resultOut").style.display = "inline";
+                document.querySelector("#divOut").style.height =`${h}px`;
+                document.querySelector("#out").innerHTML = null;
+                document.querySelector("#out").insertAdjacentElement("beforeend",
+                array.join(" "))
+                let elementOut = document.querySelector("#element");
+                elementOut.forEach(e=>{
+                    e.addEventListener("click",()=>{
+                        idV = e.getAttribute("idV");
+                        localStorage.setItem("ID VIDEO", idV);
+                    })
+                })
+            })();
+        }
+    })
 })();
