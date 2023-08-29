6fed4a67dc0ec69708e856785caf1a7a1fbb91a9
sebastianarciniegas2003 <sebastianarciniegas2003@gmail.com>
2023-08-29 09:53:10 -0500

    feat:🎨: Responsive sidebar
    Se modifico el sidebar para que sea responsive

--- src/script.js
+++ src/script.js
@@ -3,10 +3,10 @@ let side = document.querySelector("#sidebar")
 let hr = document.querySelector("#hrI")
 let cont = document.querySelector("#container");
 menu.addEventListener("click",()=>{
-    side.classList.toggle("w-[5%]");
-    side.classList.toggle("mb-6");
+    side.classList.toggle("lg:w-[5%]");
+    side.classList.toggle("lg:mb-6");
     side.classList.toggle("af");
     hr.classList.toggle("w-2/6");
     hr.classList.toggle("mb-6");
-    cont.classList.toggle("p-[20px_2%_20px_7%]");
+    cont.classList.toggle("lg:p-[20px_2%_20px_7%]");
 })
